ce3efc69ee2c223494989d37d0f38ed6ed35a658
Mohsin Khan <mkshuvo@hotmail.com>
2019-04-03 18:47:19 +0600

    You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near 's standard dummy text ever since the 1500s, when an unknown printer.' , exp_time' at line 1

--- experience.php
+++ experience.php
@@ -8,41 +8,60 @@
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     </head>     
     <body> 
-        <?php
-        require('db.php');
-        //require('auth.php');
-    ?> 
-        <div class="container">
-            <div class="row">
-                <div class="offset-3 col-md-6 ">
-                    <h3>Experience Information Form</h3>
-                    <form role="form"> 
+    <div class="container">
+        <div class="row">
+            <div class="offset-3 col-md-6 ">
+                <h3>Experience Information Form</h3>
+                        <?php
+                        require('auth.php');
+                        require('db.php');
+                        $id=$_REQUEST['id'];
+                        $query = "SELECT * FROM experience where expid='".$id."'";
+                        $result = mysqli_query($con,$query) or die ( mysqli_error($con));
+                        $row = mysqli_fetch_assoc($result);
+
+                        $status = "";
+                        if(isset($_POST['new']) && $_POST['new']==1)
+                        {
+                       
+                        
+                        $exp_title = $_REQUEST['exp_title'];
+                        $exp_description = $_REQUEST['exp_description'];
+                        $exp_time = $_REQUEST['exp_time'];
+                        $update="UPDATE experience SET exp_title='".$exp_title."' , exp_description='".$exp_description."' , exp_time='".$exp_time."'
+                        WHERE expid='".$id."'";
+                        mysqli_query($con, $update) or die ( mysqli_error($con));
+                        $status = "Record Updated Successfully. </br></br>
+                        <a href='dashboard.php'>View Updated Record</a>";
+                        echo '<p style="color:#FF0000;">'.$status.'</p>';
+                        }else {
+                        ?>
+        
+                    <form action="" method="POST" role="form"> 
+                   
+                        <input type="hidden" name="new" value="1" />
+                        <input name="expid" type="hidden" value="<?php echo $row['expid'];?>" />
                         <div class="form-group"> 
-                            <label for="exampleInputEmail1">Email address</label>                             
+                            <label for="-exp_title">exp_title</label>                             
 
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"> 
+                            <input type="text" class="form-control" name="exp_title" placeholder="Enter exp_title" value="<?php echo $row['exp_title']; ?>" /> 
                         </div>                         
 
                         <div class="form-group"> 
-                            <label for="exampleInputPassword1">Password</label>                             
+                            <label for="exp_description">exp_description</label>                             
 
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"> 
+                            <input type="text" class="form-control" name="exp_description" rows="3" placeholder="Enter exp_description" value="<?php echo $row['exp_description']; ?>" />
+                        
                         </div>                         
-
+                        
                         <div class="form-group"> 
-                            <label for="exampleInputFile">File input</label>                             
+                            <label for="exp_title">exp_time</label>                             
 
-                            <input type="file" id="exampleInputFile"> 
-                            <p class="form-text">Example block-level help text here.</p> 
-                        </div>                         
-
-                        <div class="form-check"> 
-                            <input class="form-check-input" type="checkbox" value="" id="exampleInputCheck1"> 
-                            <label class="form-check-label" for="exampleInputCheck1">                        Check me out                    </label>                             
-                        </div>                         
-
+                            <input type="text" class="form-control" name="exp_time" placeholder="Enter exp_time" value="<?php echo $row['exp_time']; ?>" /> 
+                        </div> 
                         <button type="submit" class="btn btn-primary">Submit</button>                         
                     </form>                                          
+                    <?php } ?>
                 </div>
             </div>
         </div>
